12a8ab43340253eb1bb92e4850d0be4b0cef8189
WillCMcC <willmccambley@gmail.com>
2015-10-28 21:24:19 -0700

    tweet media

--- index.js
+++ index.js
@@ -162,14 +162,17 @@ apirouter.route('/new_image')
 // takes a file upload, uploads to imgur, saves to DB
 		apirouter.route('/new_squid')
 		.post( multipartMiddleware, function(req, res, next) {
+			console.log("hit upload bish");
+
 
 			// imgur setup and upload
 			imgur.setClientId('c495aa665a64c56');
       imgur.uploadFile(req.files.file.path)
       // upload callback
 			    .then(function (json) {
-            // delete from temp memory
-            fs.unlink(req.files.file.path);
+						console.log("hit imgur bish");
+
+
             // create object to save to DB
             var squid = new Squid({
               lat : req.body.latitude,
@@ -178,20 +181,41 @@ apirouter.route('/new_image')
               });
               //  save squid
               squid.save(function (err, data) {
+								console.log("saved bish");
+
 								res.send("success");
               if (err) console.log(err);
               });
               //  tweet new squid!
               if(process.argv[2] == 'prod'){
-							client.post('statuses/update', {
-                status: 'New Squid! ' + json.data.link,
-                lat: req.body.latitude,
-                long: req.body.longitude,
-                display_coordinates: true
-               },
-               function(error, tweet, response){
-                if(error) throw error;
-              });
+								console.log("prod bish");
+								console.log(req.files)
+							var pic = fs.readFileSync(req.files.file.path);
+
+							client.post('media/upload', {media: pic}, function(error, media, response){
+								console.log("upload bish");
+								if(error){console.log(error)}
+							// If successful, a media object will be returned.
+							console.log(media);
+							// Lets tweet it
+							var status = {
+							status: 'New Squid!',
+							lat: req.body.latitude,
+							long: req.body.longitude,
+							display_coordinates: true,
+							media_ids: media.media_id_string // Pass the media id string
+						}
+
+							client.post('statuses/update', status, function(error, tweet, response){
+								console.log("tweeter bish");
+
+							if (!error) {
+							console.log(tweet);
+							}
+							fs.unlink(req.files.file.path);
+							});
+							});
+
 						}
           }
                 )
